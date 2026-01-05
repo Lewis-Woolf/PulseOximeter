1fb24963366281ee77013bc8a9871b864078f88f
Lewis-Woolf <lewiswoolf1@gmail.com>
2026-01-05 15:40:23 +0000

    better way of calculating BPM

--- PulseOximeterSingle/PulseOximeterSingle.ino
+++ PulseOximeterSingle/PulseOximeterSingle.ino
@@ -1,26 +1,26 @@
 int inPin = 10; // Pin for the heart rate reading
-long timerLength = 10000; // Length of timer for heart rate count
-long currentTimerCount = 0; // Counter for the current time
-long initialTimerCount = 0; // Counter which increases in increments of timerLength
-int beatCount = 0; // Sets the heartbeat count to 0 initially
+int BPM = 0; // Heartbeats per min
+
+long currentTimerCount = 0; // Current time
+long previousTimerCount = 0; // Time at last beat
 
 float thresholdVoltage = 5e-3; // Threshold voltage to count heart rate
 
 
 void setup() {
-
 }
 
+
 void loop() {
-  currentTimerCount = millis();
-
-  if (currentTimerCount - initialTimerCount >= timerLength) { // If an amount of time equal to timerlength has passed, initial timer is set to a multiple of timer length
-    initialTimerCount = millis();
-    beatCount = 0;
-  }
-
   float Voltage = analogRead(inPin); //Read the voltage
-  if (Voltage >= thresholdVoltage) { // When threshold voltage is reached, add 1 to the heartbeat counter
-    beatCount += 1;
+  if (Voltage >= thresholdVoltage) { // When threshold voltage is reached, calculate BPM
+    currentTimerCount = millis(); // Set current timer count
+    float timeDifference = (currentTimerCount - previousTimerCount) / 60000; // Time difference between previous and current count in minutes
+
+    BPM = 1 / timeDifference;
+
+    previousTimerCount = millis(); // Set the previous timer count to the current time AFTER the BPM is calculated
   }
+
+  Serial.println(BPM);
 }
