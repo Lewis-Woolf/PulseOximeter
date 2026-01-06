ab35e2f44f5b962c8549322a1693a6bbec4cd0df
Lewis-Woolf <lewiswoolf1@gmail.com>
2026-01-06 11:36:28 +0000

    Serial added

--- PulseOximeterSingle/PulseOximeterSingle.ino
+++ PulseOximeterSingle/PulseOximeterSingle.ino
@@ -8,13 +8,16 @@ float thresholdVoltage = 5e-3; // Threshold voltage to count heart rate
 
 
 void setup() {
+  Serial.begin(9600);
+  delay(500); // Delay for 500 ms so the serial monitor works properly
 }
 
 
 void loop() 
 {
   float Voltage = analogRead(inPin); //Read the voltage
-  if (Voltage >= thresholdVoltage) { // When threshold voltage is reached, calculate BPM
+  if (Voltage >= thresholdVoltage) // When threshold voltage is reached, calculate BPM
+  {
     currentTimerCount = millis(); // Set current timer count
     float timeDifference = (currentTimerCount - previousTimerCount) / 60000; // Time difference between previous and current count in minutes
 
